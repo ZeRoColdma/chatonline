f6d95f46b2a797d691e89380b0203b1371d12001
Zero Cold <ccarloos9@gmail.com>
2019-01-06 19:37:38 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
+  Firestore.instance.collection("mensagens").document("msg1").setData({"fron":"daniel", "texto":"Ola"});
+
   runApp(MyApp());
 }
 
