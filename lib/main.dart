1d03ee9c9dc1494851b2c8bbeddce71209be65a8
Zero Cold <ccarloos9@gmail.com>
2019-01-09 08:16:15 -0300

    Atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,175 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
-  
-  
-
-
   runApp(MyApp());
 }
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, primaryColor: Colors.purpleAccent[400]);
+
+final googleSignin = GoogleSignIn();
+final auth = FirebaseAuth.instance;
+
+_ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignin.currentUser;
+  if(user == null) {
+    user = await googleSignin.signInSilently();
+  }
+  if(user == null) async{
+    user = await googleSignin.signIn();
+  }
+  if(await auth.currentUser() == null){
+    GoogleSignInAuthentication credentials = await googleSignin.currentUser.authentication;
+    await auth.signInWithGoogle(idToken: credentials.idToken, accessToken: credentials.accessToken);
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text("Chat app"),
+            centerTitle: true,
+            elevation:
+                Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: ListView(
+              children: <Widget>[ChatMensage(), ChatMensage(), ChatMensage()],
+            )),
+            Divider(
+              height: 1,
+            ),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                        child: Text("Enviar"),
+                        onPressed: _isComposing ? () {} : null,
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: _isComposing ? () {} : null,
+                      ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMensage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://e3.365dm.com/18/11/1096x616/skynews-carlos-ghosn_4495461.jpg?20181122063005"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Carlos",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  child: Text("Teste"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
